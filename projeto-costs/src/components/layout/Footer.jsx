22b06e2954bbc3dab946b9c5054ec3f706e3c867
Victor87dev <devsjoaovvictor@gmail.com>
2024-02-02 12:02:57 -0300

    perf: desenvolvendo os componentes Navbar e Footer

---
+++ projeto-costs/src/components/layout/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa"
+import Styles from "./Footer.module.css"
+
+const Footer = ()=>{
+  return(
+    <footer className={Styles.footer}>
+        <ul className={Styles.social_list}>
+            <li>
+             <FaFacebook/>
+            </li>
+            <li>
+             <FaInstagram/>
+            </li>
+            <li>
+             <FaLinkedin/>
+            </li>  
+        </ul>
+        <p className={Styles.copy_right}><span>Costs</span> &copy; 2024</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ projeto-costs/src/components/layout/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import Container from "./Container";
+import Logo from "../../img/costs_logo.png"
+import Styles from "./Navbar.module.css"
+
+const Navbar = ()=>{
+    return(
+      
+      <nav className={Styles.navbar}>
+        <Container>
+            <Link to="/"><img src={Logo} alt="Costs" /></Link>
+            <ul className={Styles.list}>
+                <li className={Styles.item}>
+                  <Link to="/">Home</Link>
+                </li>
+                <li className={Styles.item}>
+                  <Link to="projects">Projetos</Link>
+                </li>
+                <li className={Styles.item}>
+                   <Link to="/company">Empresa</Link>
+                </li>
+                <li className={Styles.item}>
+                   <Link to="/contact">Contato</Link> 
+                </li>
+            </ul>
+        </Container>
+      </nav>
+      
+    )
+}
+
+export default Navbar
