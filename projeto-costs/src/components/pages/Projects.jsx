5b256980a1fbabd9e47afe78289bd42bae88aa0a
Victor87dev <devsjoaovvictor@gmail.com>
2024-02-12 11:53:48 -0300

    feat: criando ProjectCard, para adicionar projetos na pagina projetos

--- projeto-costs/src/components/pages/Projects.jsx
+++ projeto-costs/src/components/pages/Projects.jsx
@@ -1,11 +1,15 @@
 import { useLocation } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 import Message from "../layout/Message"
 import styles from "./Projects.module.css"
 import Container from "../layout/Container"
 import LinkButton from "../layout/LinkButton"
+import ProjectCard from "../project/ProjectCard"
 
 const Projects = ()=>{
+
+  const [projects, setProjects] = useState([])
 
   const location = useLocation()
 
@@ -15,6 +19,20 @@ const Projects = ()=>{
     message = location.state.message
   }
 
+  useEffect(()=>{
+    fetch('http://localhost:5000/projects',{
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    })
+    .then((resp)=>resp.json())
+    .then(data => {
+      setProjects(data)
+    })
+    .catch((err)=>console.log(err))
+  }, [])
+
   return(
     <div className={styles.project_container}>
       <div className={styles.title_container}>
@@ -23,7 +41,16 @@ const Projects = ()=>{
       </div>
       {message && <Message  type="sucess" msg={message}/>}
       <Container customClass="start">
-        <p>Projetos...</p>
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard 
+            id={project.id}
+            name={project.name}
+            budget={project.budget}
+            category={project.category.name}
+            key={project.id}
+            />
+          ))}
       </Container>
     </div>
   )
---
+++ projeto-costs/src/components/project/ProjectCard.jsx
@@ -0,0 +1,15 @@
+import styles from "./ProjectCard.module.css"
+import { BsPencil, BsFillTrashFill } from "react-icons/bs"
+
+const ProjectCard = ({id, name, budget, category, handleRemove})=>{
+    return(
+        <div>
+            <h4>{name}</h4>
+            <p>
+               
+            </p>
+        </div>
+    )
+}
+
+export default ProjectCard
