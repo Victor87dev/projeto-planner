40730197b1aa4bcaa0290808879722beb65c1fe7
Victor87dev <devsjoaovvictor@gmail.com>
2024-02-15 11:51:27 -0300

    feat: criando page de edição, para editar um projeto criado

--- projeto-costs/src/App.jsx
+++ projeto-costs/src/App.jsx
@@ -4,11 +4,12 @@ import Contact from "./components/pages/Contact"
 import Company from "./components/pages/Company"
 import NewProject from "./components/pages/NewProject"
 import Projects from "./components/pages/Projects"
-
+import Project from "./components/pages/Project"
 
 import Container from "./components/layout/Container"
 import Navbar from "./components/layout/Navbar"
 import Footer from "./components/layout/Footer"
+
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
             <Route path="/company" element={<Company />}/>
             <Route path="/contact" element={<Contact />}/>
             <Route path="/newproject" element={<NewProject />}/>
+            <Route path="/project/:id" element={<Project />}/>
           
         </Routes>
       </Container>
