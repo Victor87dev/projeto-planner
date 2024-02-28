083c99b44a6d345858e8878d6c9cca77e38856bb
Victor87dev <devsjoaovvictor@gmail.com>
2024-02-28 18:22:24 -0300

    feat: adicionando responsividade no navbar

--- projeto-costs/src/components/layout/Navbar.jsx
+++ projeto-costs/src/components/layout/Navbar.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom"
 import Container from "./Container";
 import Logo from "../../img/img_costs.png"
 import Styles from "./Navbar.module.css"
 
-const Navbar = ()=>{
+class Navbar extends Component {
+
+  state = { clicked: false };
+  handleClick = () =>{
+    this.setState({clicked:
+      !this.state.clicked})
+  }
+  render() {
     return(
-      
       <nav className={Styles.navbar}>
         <Container>
             <Link to="/"><img className={Styles.img_item} src={Logo} alt="Costs" /></Link>
@@ -25,9 +31,18 @@ const Navbar = ()=>{
                 </li>
             </ul>
         </Container>
+        <div className={Styles.mobile} onClick={this.handleClick}>
+          <i id="bar"
+          className={
+            this.state.clicked ? 
+            'fas fa-times' : 'fas fa-bars'
+          } 
+          ></i>
+        </div>
       </nav>
       
     )
+}
 }
 
 export default Navbar
