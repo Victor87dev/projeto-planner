51d3fc5993e7594881928d134f60ce2e8dc97d8e
Victor87dev <devsjoaovvictor@gmail.com>
2024-03-13 15:08:12 -0300

    feat: fazendo verificação dos campos de criação de serviço

--- projeto-costs/src/components/service/ServiceForm.jsx
+++ projeto-costs/src/components/service/ServiceForm.jsx
@@ -4,13 +4,29 @@ import styles from "../project/ProjectForm.module.css"
 
 import Input from "../form/Input"
 import SubmitButton from "../form/SubmitButton"
+import Message from "../layout/Message"
 
 const ServiceForm = ({ handleSubmit, btnText, projectData })=>{
 
    const [service, setService] = useState({})
+   const [message, setMessage] = useState()
+   const [type, setType] = useState()
 
    function submit(e){
+    if(service.name === undefined || service.cost === undefined || service.description === undefined || service.name === '' || service.cost === '' || service.description === ''){
+      e.preventDefault()
+      setMessage('Informações incompletas, certifique-se de completar as informações!')
+      setType('error')
+
+      setTimeout(()=>{
+        setMessage('')
+      }, 2400)
+
+      return false 
+    }
+
      e.preventDefault()
+
      projectData.services.push(service)
      handleSubmit(projectData)
    }
@@ -19,7 +35,8 @@ const ServiceForm = ({ handleSubmit, btnText, projectData })=>{
      setService({...service, [e.target.name]: e.target.value})
    }
 
-   return(
+   return(<>
+    {message && <Message type={type} msg={message}/>}
     <form onSubmit={submit} className={styles.form}>
         <Input 
          type="text"
@@ -44,6 +61,7 @@ const ServiceForm = ({ handleSubmit, btnText, projectData })=>{
         />
         <SubmitButton text={btnText} />
     </form>
+    </>
    )
 
 }
